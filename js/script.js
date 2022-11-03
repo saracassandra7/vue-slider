b629e0c72b61b32930d4b66c26028b1e81deaf4f
saracassandra7 <saracassandra7@gmail.com>
2022-11-03 16:31:35 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,3 @@
+/*Partendo dal markup della versione svolta in js plain, rifare lo slider ma questa volta usando Vue.
+- al click su una thumb, visualizzare in grande l’immagine corrispondente
+- aggiungere la classe active alla thumb attiva*/
